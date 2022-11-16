0dd57ab20982c96917cf24da34f63338a597ed9e
Bingoblast <243mjparth@gmail.com>
2022-11-16 21:39:29 +0530

    Its

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ const diceEL = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
 
 score0EL.textContent = "0";
 score1EL.textContent = "0";
 diceEL.classList.add("hidden");
 
+let scores = [0, 0];
+let currentScore = 0;
+let activeScore = 0;
 //Roll the dice
 btnRoll.addEventListener("click", function () {
   //1. Roll the random dice number
@@ -20,5 +25,14 @@ btnRoll.addEventListener("click", function () {
   diceEL.classList.remove("hidden");
   diceEL.src = `dice-${dice}.png`;
 
-  //2.check for dice roll to 1 or not
+  //3.check for dice roll to 1 or not
+  if (dice != 1) {
+    //Add current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    //Switch Player
+    document.querySelector(".player--1").classList.add("player--active");
+    document.querySelector(".player--0").classList.remove("player--active");
+  }
 });
